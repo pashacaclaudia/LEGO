22b58cd596e477addaab0f1d6351ce4a75657678
pashacaclaudia <pashaca.claudia.carolina@iisgalvanimi.edu.it>
2019-11-28 13:49:01 +0000

    Non funziona

---
+++ ESERCIZIOLEGO/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const people = require('./projects.json'); //Copia il file people.json dentro la variabile people
+const app = express();
+
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Homepage',
+    people: people.profiles //Passa il vettore profiles alla pagina index.pug
+  });
+});
+
+app.get('/profile', (req, res) => {
+  const person = people.profiles.find((p) => p.id === req.query.id);
+  res.render('profile', {
+    title: `About ${person.firstname} ${person.lastname}`,
+    person,
+  });
+});
+
+app.listen(3000, function () {
+ console.log('Example app listening on port 3000!');
+});
